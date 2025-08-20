9346b9b8222ec6b44132df1b42fea47166d46459
MrMoudDev <santiago.velasquez08120@gmail.com>
2025-08-20 00:52:53 -0500

    Terminamos el login con sus validaciones y mensajes de error

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators, ɵInternalFormsSharedModule } from "@angular/forms";
+
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ɵInternalFormsSharedModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
 
+  formData = new FormGroup({
+    correo: new FormControl( '', [Validators.required, Validators.email]),
+    password: new FormControl( '', [Validators.required, Validators.minLength(8), Validators.maxLength(12)]),
+  });
+
+  loginSubmit(){
+    if (this.formData.valid) {
+      console.log(this.formData.value)
+    }
+
+    this.formData.reset()
+  }
 }
